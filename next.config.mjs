d57eb85e8726e5bbf7f0f806c7cd289090c23326
Runkang Chen <chen.runkang10@gmail.com>
2024-07-29 17:47:04 +0200

    [Update]

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        staleTimes: {
+            static: 30,
+            dynamic: 180,
+        }
+    },
+    reactStrictMode: true
+};
 
 export default nextConfig;
