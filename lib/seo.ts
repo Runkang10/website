e4d00ab2f6c785d336e3a62051e8e9ff360f77ef
Runkang Chen <chen.runkang10@gmail.com>
2024-10-26 19:07:27 +0200

    feat: Moved and updated description from layout

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,5 +1,9 @@
+import { getCurrentYear } from "./utils";
+
 const Title = "Runkang10";
 const TitleTemplate = `%s - ${Title}`;
-const Description = "Hi, I'm Runkang10, founder of SyS App.";
+const Description = `Hi, I'm Runkang10, a ${
+  getCurrentYear() - 2010
+} years old Python, JavaScript (TypeScript), Rust, Java and Kotlin developer.\nI am the founder of SyS App.`;
 
 export { Title, TitleTemplate, Description };
