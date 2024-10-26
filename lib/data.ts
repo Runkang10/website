2c9f474723c8a63d16473363640a2442bc6d20b4
Runkang Chen <chen.runkang10@gmail.com>
2024-10-26 18:54:29 +0200

    feat: Add new version formater Inspired from Modrinth

--- lib/data.ts
+++ lib/data.ts
@@ -2,24 +2,39 @@
 import path from "path";
 import fs from "fs";
 
+const getCurrentBranch = () => {
+  const head = fs
+    .readFileSync(path.join(process.cwd(), ".git/HEAD"), "utf8")
+    .trim();
+  return path.basename(head.split(" ")[1]);
+};
+
 const getCommitID = (): {
-  ok: boolean,
-  id: string
+  ok: boolean;
+  id: string;
 } => {
   try {
-    const head = fs.readFileSync(path.join(process.cwd(), '.git/HEAD'), 'utf8').trim();
-    const refPath = head.split(' ')[1];
-    const CommitId = fs.readFileSync(path.join(process.cwd(), '.git', refPath), 'utf8').trim();
+    const head = fs
+      .readFileSync(path.join(process.cwd(), ".git/HEAD"), "utf8")
+      .trim();
+    const refPath = head.split(" ")[1];
+    const CommitId = fs
+      .readFileSync(path.join(process.cwd(), ".git", refPath), "utf8")
+      .trim();
     return {
       ok: true,
-      id: CommitId
+      id: CommitId,
     };
   } catch {
     return {
       ok: false,
-      id: "Unknown"
-    }
+      id: "Unknown",
+    };
   }
 };
 
-export { getCommitID };
+const getFormatedCommitBasedVersion = () => {
+  const CommitId = getCommitID();
+};
+
+export { getCurrentBranch, getCommitID, getFormatedCommitBasedVersion };
