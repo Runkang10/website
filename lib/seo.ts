cf8bb735bce6853089cba9052e9b3b83708cede7
Runkang Chen <chen.runkang10@gmail.com>
2024-10-29 18:28:55 +0100

    feat: Updated SEO description

--- lib/seo.ts
+++ lib/seo.ts
@@ -1,9 +1,23 @@
+import { ProgrammingLanguagesType } from "@/types/tech";
 import { getCurrentYear } from "./utils";
+import { ProgrammingLanguages } from "@/content/tech";
 
 const Title = "Runkang10";
 const TitleTemplate = `%s - ${Title}`;
+
+const formatedProgrammingLanguages = (
+  languages: ProgrammingLanguagesType[]
+): string => {
+  return languages
+    .map((lang) => lang.name)
+    .join(", ")
+    .replace(/, ([^,]+)$/, " and $1");
+};
+
 const Description = `Hi, I'm Runkang10, a ${
   getCurrentYear() - 2010
-} years old Python, JavaScript (TypeScript), Rust, Java and Kotlin developer.\nI am the founder of SyS App.`;
+} years old ${formatedProgrammingLanguages(
+  ProgrammingLanguages
+)} developer.\nI am the founder of SyS App.`;
 
 export { Title, TitleTemplate, Description };
