901a4fa3fd71da4d87196e87062605f4b2a4ab7b
Runkang Chen <chen.runkang10@gmail.com>
2024-10-29 18:26:39 +0100

    fix: Update code with new homepage navbar style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,33 +1,33 @@
+import { MainBox } from "@/components/MainBox";
 import { Metadata } from "next";
 
 const metadata: Metadata = {
+  title: "404",
+  description: "This page could not be found.",
+  openGraph: {
     title: "404",
     description: "This page could not be found.",
-    openGraph: {
-        title: "404",
-        description: "This page could not be found."
-    },
-    twitter: {
-        title: "404",
-        description: "This page could not be found."
-    }
-}
+  },
+  twitter: {
+    title: "404",
+    description: "This page could not be found.",
+  },
+};
 
 const E404 = () => {
-    return (
-        <div className="flex items-center justify-center flex-1">
-            <div className="flex flex-col gap-4">
-                <h1 className="text-3xl text-center font-semibold">
-                    404
-                </h1>
-                <p className="text-sm lg:text-base text-center font-normal">
-                    This page could not be found.
-                </p>
-            </div>
+  return (
+    <MainBox includeNavBar>
+      <div className="flex-1 flex items-center justify-center">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-3xl text-center font-semibold">404</h1>
+          <p className="text-sm lg:text-base text-center font-normal">
+            This page could not be found.
+          </p>
         </div>
-    )
-}
-
+      </div>
+    </MainBox>
+  );
+};
 
 export { metadata };
 export default E404;
