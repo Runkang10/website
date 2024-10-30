22f4defcc3a926ce329107f05608dcfb74b40250
Runkang Chen <chen.runkang10@gmail.com>
2024-10-30 13:46:34 +0100

    chore: Coming soon

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,14 @@
+import { MainBox } from "@/components/MainBox";
 import { Metadata } from "next";
 
 const metadata: Metadata = {
-    title: "About",
-    description: "About me."
-}
+  title: "About",
+  description: "About me.",
+};
 
 const About = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return <MainBox includeNavBar></MainBox>;
+};
 
 export { metadata };
 export default About;
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,17 +1,13 @@
+import { MainBox } from "@/components/MainBox";
 import { Metadata } from "next";
 
-
 const metadata: Metadata = {
-    title: "Contact",
-    description: "Contact."
-}
+  title: "Contact Me",
+};
 
 const Contact = () => {
-    return (
-        <>
-        </>
-    )
-}
+  return <MainBox includeNavBar></MainBox>;
+};
 
 export { metadata };
 export default Contact;
