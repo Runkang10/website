ae7c335b65ab4cbf6e1b02e2fcbc66ab88891ae0
Runkang Chen <chen.runkang10@gmail.com>
2024-10-30 13:46:16 +0100

    chore: Coming soon

---
+++ enums/link.ts
@@ -0,0 +1,13 @@
+enum LinkType {
+  INTERNAL,
+  EXTERNAL,
+}
+
+enum LinkStyle {
+  LINK_INTERNAL = "",
+  LINK_EXTERNAL = "",
+  LINK_NO_HOVER_INTERNAL = "",
+  LINK_HOVER_EXTERNAL = ""
+}
+
+export { LinkType, LinkStyle };
